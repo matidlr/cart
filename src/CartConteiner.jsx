a5b32325b44cd5f36f4069c5201596dbdb700a52
Matias <mati.delarosa1984@gmail.com>
2024-02-16 16:17:54 -0300

    decrease amount

--- src/CartConteiner.jsx
+++ src/CartConteiner.jsx
@@ -1,9 +1,8 @@
 import CartItem from './CartItem';
+import { useGlobalContext } from './context';
 import cartItems from './data';
-import { useGlobalContext } from './context'
-
 const CartContainer = () => {
-  const {cart, clearCart} = useGlobalContext();
+  const { cart, clearCart, totalCost } = useGlobalContext();
 
   const cartArray = Array.from(cart.entries());
 
@@ -27,7 +26,7 @@ const CartContainer = () => {
       {/* cart items */}
       <div>
         {cartArray.map((cartItem) => {
-          const [id,item] = cartItem
+          const [id, item] = cartItem;
           return <CartItem key={id} {...item} />;
         })}
       </div>
@@ -36,13 +35,10 @@ const CartContainer = () => {
         <hr />
         <div>
           <h5 className='cart-total'>
-            total <span>$10</span>
+            total <span>${totalCost.toFixed(2)}</span>
           </h5>
         </div>
-        <button
-          className='btn btn-hipster'
-          onClick={clearCart}
-        >
+        <button className='btn btn-hipster' onClick={clearCart}>
           clear cart
         </button>
       </footer>
