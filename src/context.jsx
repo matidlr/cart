3af0bbfe1a717636f4b6746aa991436ab1c9399c
Matias <mati.delarosa1984@gmail.com>
2024-02-16 17:19:40 -0300

    calculate totals

--- src/context.jsx
+++ src/context.jsx
@@ -8,6 +8,7 @@ import { CLEAR_CART,
          LOADING, 
          DISPLAY_ITEMS} 
          from './actions'
+import { getTotals } from './utils';
 
 const AppContext = createContext();
 
@@ -20,16 +21,26 @@ const initialState = {
 
 export const AppProvider = ({children}) => {
   const [state,dispatch] = useReducer (reducer, initialState);
-  
+  const {totalAmount, totalCost } = getTotals(state.cart);
+
+
 const clearCart = () =>{
   dispatch({ type: CLEAR_CART });
 }
 
 const remove = (id) => {
-  dispatch({ type: REMOVE,payload: { id }})
+  dispatch({ type: REMOVE,payload: { id }});
 }
 
-  return <AppContext.Provider value={{  }}>
+const increase = (id) => {
+  dispatch({ type:INCREASE, payload: { id }});
+}
+
+const decrease = (id) => {
+  dispatch({ type:DECREASE, payload: { id }});
+}
+
+  return <AppContext.Provider value={{ ...state, clearCart, remove, increase, decrease, totalAmount, totalCost  }}>
  {children}
   </AppContext.Provider>
 }
